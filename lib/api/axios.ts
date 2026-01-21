b362366e396310dcc1384f4f4b70b25db85abbbc
Samir <khadkasamir8079@gmail.com>
2026-01-21 17:37:19 +0545

    feat: add withCredentials and JWT bearer token interceptor to axios instance

--- lib/api/axios.ts
+++ lib/api/axios.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import Cookies from "js-cookie";
 
 const BASE_URL = process.env.NEXT_PUBLIC_API_URL
     || "http://localhost:5050";
@@ -8,6 +9,16 @@ const axiosInstance = axios.create({
     headers: {
         "Content-Type": "application/json",
     },
+    withCredentials: true,
+});
+
+// Attach JWT from cookie to every request
+axiosInstance.interceptors.request.use((config) => {
+    const token = Cookies.get("authToken");
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
 });
 
 export default axiosInstance;
