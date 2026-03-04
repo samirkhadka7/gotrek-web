4da05325a1a35399ed8f6e96f0c0f2b707cb6a8b
Samir <khadkasamir8079@gmail.com>
2026-03-05 00:17:03 +0545

    test(client): add Jest test configuration

---
+++ client/jest.config.js
@@ -0,0 +1,16 @@
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({ dir: './' });
+
+/** @type {import('jest').Config} */
+const config = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/__tests__/setup.tsx'],
+  testMatch: ['<rootDir>/src/__tests__/**/*.test.{ts,tsx}'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+};
+
+module.exports = createJestConfig(config);
